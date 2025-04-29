9e1a88502f78651fd57f239fb86cdc4278ab19ed
sertdfyguhi-git <gdkamtalan@gmail.com>
2025-04-29 14:46:42 +0800

    add proxy2

--- js/extra.js
+++ js/extra.js
@@ -1,7 +1,18 @@
+const CORS_PROXY = [
+  "https://api.allorigins.win/raw?url=",
+  "https://api.cors.lol/?url=",
+  "https://corsproxy.io/?url=",
+  "https://api.codetabs.com/v1/proxy?quest=",
+];
+
 let response = "";
 
 function checkWebsite() {
-  fetch("https://api.cors.lol/?url=https://pastebin.com/raw/AbUUxLR1") // Replace with your target URL
+  fetch(
+    `${
+      CORS_PROXY[Math.floor(Math.random() * CORS_PROXY.length)]
+    }https://pastebin.com/raw/AbUUxLR1`
+  ) // Replace with your target URL
     .then(response => response.text())
     .then(data => {
       if (data != response) {
